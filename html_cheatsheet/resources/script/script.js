198f17fc4a87fa2e792be80e24bec591613a7ee8
Mirica Dragos-Neculai <nd.mirica@gmail.com>
2020-08-07 22:28:57 +0300

    some upgrades

--- html_cheatsheet/resources/script/script.js
+++ html_cheatsheet/resources/script/script.js
@@ -1,5 +1,9 @@
 //Hide/Show the content map elements
+//We select the element with id "hide-this", that is the parent of the elements that we need to change
 let hiding = document.getElementById('hide-this');
+// We create the function that is used to change the html elements, we pass "e" to this function because it is the only way to know which element is clicked inside the parent to which we assign it, once passed "e" every time an element inside this parent or even the parent is clicked, "e" will take the value of that element and through e.target we can select and modify it.
+//e.target.tagName returns the exact tag name to us,
+//e.target.nextElementSibling instead selects the next element to the one clicked
 function hideElement(e){
 	if(e.target.tagName.toLowerCase() === 'span' && e.target.nextElementSibling.hidden === true){
 		e.target.nextElementSibling.hidden=false;
