dd026697074aefe5444d97ab9e88ff82c8710cac
Mirica Dragos-Neculai <nd.mirica@gmail.com>
2020-08-07 18:27:38 +0300

    open/close js nav added

--- js_cheatsheet/resources/script/script.js
+++ js_cheatsheet/resources/script/script.js
@@ -1,6 +1,24 @@
-//Testing onclick
+//Testing onclick/addEventListener
 let surprize = document.getElementById('surprize');
-function surprizeIt(){
+let surprizeIt = function () {
 	surprize.classList.add("continuos-changing-color");
 }
+/* Whit onclick
 surprize.onclick = surprizeIt;
+*/
+
+//with addEventListener
+surprize.addEventListener('click', surprizeIt);
+
+
+
+//Hide/Show the content map elements
+let hiding = document.getElementById('hide-this');
+function hideElement(e){
+	if(e.target.tagName.toLowerCase() === 'span' && e.target.nextElementSibling.hidden === true){
+		e.target.nextElementSibling.hidden=false;
+	} else if (e.target.tagName.toLowerCase() === 'span' && e.target.nextElementSibling.hidden === false){
+		e.target.nextElementSibling.hidden=true;
+	}
+}
+hiding.addEventListener('click', hideElement);
