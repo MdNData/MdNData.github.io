ec78da56c85ac230fa19b5ca15ca2cc97b88d149
Mirica Dragos-Neculai <nd.mirica@gmail.com>
2020-08-07 19:14:03 +0300

    Big design improvement

---
+++ html_cheatsheet/resources/script/script.js
@@ -0,0 +1,10 @@
+//Hide/Show the content map elements
+let hiding = document.getElementById('hide-this');
+function hideElement(e){
+	if(e.target.tagName.toLowerCase() === 'span' && e.target.nextElementSibling.hidden === true){
+		e.target.nextElementSibling.hidden=false;
+	} else if (e.target.tagName.toLowerCase() === 'span' && e.target.nextElementSibling.hidden === false){
+		e.target.nextElementSibling.hidden=true;
+	}
+}
+hiding.addEventListener('click', hideElement);
