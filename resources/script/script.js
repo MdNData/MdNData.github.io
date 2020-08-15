fed5bde4e34c8c6bb7be0070ec50800c83f2d2f6
Mirica Dragos-Neculai <nd.mirica@gmail.com>
2020-08-15 13:35:32 +0300

    added functionality to main index

---
+++ resources/script/script.js
@@ -0,0 +1,20 @@
+//Change top nav classes if the page is crolled
+//Select the elements
+var topNav = document.getElementById('navigation');
+var topNavSub = document.getElementById('resources-sub');
+//Function that is run with the Scroll event
+function changeNav() {
+  if (window.pageYOffset > 10) {
+    topNav.classList.add("nav-colored");
+    topNav.classList.remove("nav-transparent");
+    topNavSub.classList.add("nav-colored");
+    topNavSub.classList.remove("nav-transparent");
+  } else {
+    topNav.classList.add("nav-transparent");
+    topNav.classList.remove("nav-colored");
+    topNavSub.classList.add("nav-transparent");
+    topNavSub.classList.remove("nav-colored");
+  }
+}
+//add event to the window
+window.addEventListener("scroll", changeNav);
