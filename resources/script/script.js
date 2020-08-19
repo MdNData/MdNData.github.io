1e6beb6dce82c040c5f5b03f634c0d1a92d33787
Mirica Dragos-Neculai <nd.mirica@gmail.com>
2020-08-19 21:08:19 +0300

    added social facebook

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,19 +1,24 @@
 //Change top nav classes if the page is crolled
 //Select the elements
 var topNav = document.getElementById('navigation');
-var topNavSub = document.getElementById('resources-sub');
+var topNavSub1 = document.getElementById('resources-sub1');
+var topNavSub2 = document.getElementById('resources-sub2');
 //Function that is run with the Scroll event
 function changeNav() {
   if (window.pageYOffset > 10) {
     topNav.classList.add("nav-colored");
     topNav.classList.remove("nav-transparent");
-    topNavSub.classList.add("nav-colored");
-    topNavSub.classList.remove("nav-transparent");
+    topNavSub1.classList.add("nav-colored");
+    topNavSub1.classList.remove("nav-transparent");
+    topNavSub2.classList.add("nav-colored");
+    topNavSub2.classList.remove("nav-transparent");
   } else {
     topNav.classList.add("nav-transparent");
     topNav.classList.remove("nav-colored");
-    topNavSub.classList.add("nav-transparent");
-    topNavSub.classList.remove("nav-colored");
+    topNavSub1.classList.add("nav-transparent");
+    topNavSub1.classList.remove("nav-colored");
+    topNavSub2.classList.add("nav-transparent");
+    topNavSub2.classList.remove("nav-colored");
   }
 }
 //add event to the window
