ef595b7421c9e096e2561b7d227a9f24c4d845a4
Mirica Dragos-Neculai <nd.mirica@gmail.com>
2020-08-20 15:19:18 +0300

    little bug fixed

--- html_cheatsheet/resources/script/script.js
+++ html_cheatsheet/resources/script/script.js
@@ -1,7 +1,7 @@
 //Hide/Show the content map elements
 //We select the element with id "hide-this", that is the parent of the elements that we need to change
 let hiding = document.getElementById('hide-this');
-var indexMenuHide = document.getElementById('open-index').nextElementSibling;
+var indexMenuHide = document.getElementById('open-index');
 // We create the function that is used to change the html elements, we pass "e" to this function because it is the only way to know which element is clicked inside the parent to which we assign it, once passed "e" every time an element inside this parent or even the parent is clicked, "e" will take the value of that element and through e.target we can select and modify it.
 //e.target.tagName returns the exact tag name to us,
 //e.target.nextElementSibling instead selects the next element to the one clicked
@@ -11,7 +11,10 @@ function hideElement(e){
 	} else if (e.target.tagName.toLowerCase() === 'span' && e.target.nextElementSibling.hidden === false){
 		e.target.nextElementSibling.hidden=true;
 	} else if (e.target.parentElement.parentElement.hidden===false){
-        indexMenuHide.style.display = "none";
+        indexMenuHide.nextElementSibling.style.display = "none";
+        indexMenuHide.classList.remove('fa-times');
+        indexMenuHide.classList.add('fa-stream');
+        indexMenuHide.style.fontSize = "8vw";
     }
 }
 hiding.addEventListener('click', hideElement);
--- js_cheatsheet/resources/script/script.js
+++ js_cheatsheet/resources/script/script.js
@@ -13,12 +13,22 @@ surprize.addEventListener('click', surprizeIt);
 
 
 //Hide/Show the content map elements
+//We select the element with id "hide-this", that is the parent of the elements that we need to change
 let hiding = document.getElementById('hide-this');
+var indexMenuHide = document.getElementById('open-index');
+// We create the function that is used to change the html elements, we pass "e" to this function because it is the only way to know which element is clicked inside the parent to which we assign it, once passed "e" every time an element inside this parent or even the parent is clicked, "e" will take the value of that element and through e.target we can select and modify it.
+//e.target.tagName returns the exact tag name to us,
+//e.target.nextElementSibling instead selects the next element to the one clicked
 function hideElement(e){
 	if(e.target.tagName.toLowerCase() === 'span' && e.target.nextElementSibling.hidden === true){
 		e.target.nextElementSibling.hidden=false;
-	} else if (e.target.tagName.toLowerCase() === 'span'){
+	} else if (e.target.tagName.toLowerCase() === 'span' && e.target.nextElementSibling.hidden === false){
 		e.target.nextElementSibling.hidden=true;
-	}
+	} else if (e.target.parentElement.parentElement.hidden===false){
+        indexMenuHide.nextElementSibling.style.display = "none";
+        indexMenuHide.classList.remove('fa-times');
+        indexMenuHide.classList.add('fa-stream');
+        indexMenuHide.style.fontSize = "8vw";
+    }
 }
 hiding.addEventListener('click', hideElement);
