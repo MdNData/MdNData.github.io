026c5314f31337064cda8d15bff14038527ec08d
Mirica Dragos-Neculai <nd.mirica@gmail.com>
2020-08-20 12:47:27 +0300

    menu index added

--- resources/script/script.js
+++ resources/script/script.js
@@ -5,20 +5,22 @@ var topNavSub1 = document.getElementById('resources-sub1');
 var topNavSub2 = document.getElementById('resources-sub2');
 //Function that is run with the Scroll event
 function changeNav() {
-  if (window.pageYOffset > 10) {
-    topNav.classList.add("nav-colored");
-    topNav.classList.remove("nav-transparent");
-    topNavSub1.classList.add("nav-colored");
-    topNavSub1.classList.remove("nav-transparent");
-    topNavSub2.classList.add("nav-colored");
-    topNavSub2.classList.remove("nav-transparent");
-  } else {
-    topNav.classList.add("nav-transparent");
-    topNav.classList.remove("nav-colored");
-    topNavSub1.classList.add("nav-transparent");
-    topNavSub1.classList.remove("nav-colored");
-    topNavSub2.classList.add("nav-transparent");
-    topNavSub2.classList.remove("nav-colored");
+  if(window.innerHeight < window.innerWidth){
+    if (window.pageYOffset > 10) {
+      topNav.classList.add("nav-colored");
+      topNav.classList.remove("nav-transparent");
+      topNavSub1.classList.add("nav-colored");
+      topNavSub1.classList.remove("nav-transparent");
+      topNavSub2.classList.add("nav-colored");
+      topNavSub2.classList.remove("nav-transparent");
+    } else {
+      topNav.classList.add("nav-transparent");
+      topNav.classList.remove("nav-colored");
+      topNavSub1.classList.add("nav-transparent");
+      topNavSub1.classList.remove("nav-colored");
+      topNavSub2.classList.add("nav-transparent");
+      topNavSub2.classList.remove("nav-colored");
+    }
   }
 }
 //add event to the window
@@ -41,12 +43,16 @@ closeMenuNav.addEventListener("click", function(){
 let showing = document.getElementById('menu-nav');
 function hideElement(e){
   if(window.innerHeight > window.innerWidth){
-    if(e.target.tagName.toLowerCase() === 'a' && e.target.nextElementSibling.style.display==="inline-flex"){
+    if((e.target.tagName.toLowerCase() === 'a' || e.target.nextElementSibling.tagName.toLowerCase() === "section") && e.target.nextElementSibling.style.display==="inline-flex"){
       e.target.nextElementSibling.style.display="none";
-    } else if (e.target.tagName.toLowerCase() === 'a'){
+    } else if (e.target.tagName.toLowerCase() === 'a' || e.target.nextElementSibling.tagName.toLowerCase() === "section"){
       e.target.nextElementSibling.style.display="inline-flex";
     }
   }
 }
 showing.addEventListener('click', hideElement);
 
+
+//Make headers index menu appear/disappear
+var indexMenu = document.getElementById('open-index');
+indexMenu.addEventListener('click', hideElement);
